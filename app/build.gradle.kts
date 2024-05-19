2fc908a4465f1e5609866fe3ed4cd0a5b18cab1c
Jules <julespouvreaux@gmail.com>
2024-05-19 18:17:00 +0200

    1.0.3
    Took 17 seconds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
 		applicationId = "fr.angel.soundtap"
 		minSdk = 30
 		targetSdk = 34
-		versionCode = 12
-		versionName = "1.0.2"
+		versionCode = 13
+		versionName = "1.0.3"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 		vectorDrawables {
