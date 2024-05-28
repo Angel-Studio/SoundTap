675b02c82e3773709c73ff811426faad6f474bcb
Jules <julespouvreaux@gmail.com>
2024-05-28 18:22:53 +0200

    Quick fixes
    Took 48 seconds

--- app/src/main/java/fr/angel/soundtap/ColorExt.kt
+++ app/src/main/java/fr/angel/soundtap/ColorExt.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2024 Angel Studio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package fr.angel.soundtap
 
 import androidx.compose.ui.graphics.Color
@@ -38,4 +53,3 @@ fun Color.hue(): Float {
 	}
 	return (hue * 60.0).toFloat() // hue is in [0,6], scale it up
 }
-
--- app/src/main/java/fr/angel/soundtap/ui/app/CustomizationScreen.kt
+++ app/src/main/java/fr/angel/soundtap/ui/app/CustomizationScreen.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2024 Angel Studio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package fr.angel.soundtap.ui.app
 
 import android.graphics.RenderEffect
@@ -302,27 +317,27 @@ fun SharedTransitionScope.CustomizationScreen(
 												val shader =
 													remember { RuntimeShader(PERLIN_NOISE) }
 
-											if (selected && item != HapticFeedbackLevel.NONE) {
-												Modifier
-													.onSizeChanged { size ->
-														shader.setFloatUniform(
-															"resolution",
-															size.width.toFloat() * (0.4f + 0.2f * index),
-															size.height.toFloat() * (0.4f + 0.2f * index)
-														)
-													}
-													.graphicsLayer {
-														shader.setFloatUniform("time", time)
-														renderEffect = RenderEffect
-															.createRuntimeShaderEffect(
-																shader,
-																"contents"
+												if (selected && item != HapticFeedbackLevel.NONE) {
+													Modifier
+														.onSizeChanged { size ->
+															shader.setFloatUniform(
+																"resolution",
+																size.width.toFloat() * (0.4f + 0.2f * index),
+																size.height.toFloat() * (0.4f + 0.2f * index)
 															)
-															.asComposeRenderEffect()
-													}
-											} else {
-												Modifier
-											}
+														}
+														.graphicsLayer {
+															shader.setFloatUniform("time", time)
+															renderEffect = RenderEffect
+																.createRuntimeShaderEffect(
+																	shader,
+																	"contents"
+																)
+																.asComposeRenderEffect()
+														}
+												} else {
+													Modifier
+												}
 											} else {
 												Modifier
 											}
